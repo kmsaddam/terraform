6826ad3545fcbf349841677a6999341e28e32912
K M Saddam Hossain <dev.saddam@gmail.com>
2026-01-18 13:21:07 +0600

    Added Multiple Files

--- first-terraform.tf
+++ first-terraform.tf
@@ -1,14 +1,21 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAVWJGBYJDGP6CIMN2"
-    secret_key = "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"
 }
 
 resource "aws_instance" "MyFirstEC2" {
+    count = 2
     ami = "ami-02dc6e3e481e2bbc5"
     instance_type = "t2.micro"
     key_name = "TerraFormLive"
     tags = {
       Name = "TerraformAssignment"
     }
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+   bucket = "my-s3-test-bucket02"
+   force_destroy = true
+   tags = {
+     Name = "TerraformAssignment"
+   }
 }
---
+++ terraform.tfvars
@@ -0,0 +1,19 @@
+# Credintials
+# -------------------------
+# access_key = "AKIAVWJGBYJDGP6CIMN2"
+# secret_key = "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"
+
+# EC2 Variables
+# -------------------------
+ami = "ami-02dc6e3e481e2bbc5"
+instance_type = "t2.micro"
+key_name = "TerraFormLive"
+tags = {
+    Name = "TerraformAssignment"
+}
+
+# S3 Bucket Variables
+# -------------------------
+
+bucket = "my-s3-assignment-bucket"
+force_destroy = true
